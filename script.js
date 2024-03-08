cf31b3b40b1d2362842e587495099917f3047150
usha2489 <128898127+usha2489@users.noreply.github.com>
2024-03-08 21:23:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+let myResume={
+    "basics": {
+      "name": "USHA",
+      "email": "usha2489@yahoo.com",
+      "phone": 123456789,
+      "degree": "diploma",
+      "location": {
+        "address": "312 mysore hebbal",
+        "postalCode": 570016,
+        "city": "mysore",
+        "state": "karnataka",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "https://www.naukri.com/job-listings-050324004832",
+          "github":"https://github.com/usha2489/USHA"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "wipro lts",
+        "position": "sr.application eng",
+        "startDate": "2014-07-04",
+        "endDate": "2021-08-15",
+        "summary": "oracle support",
+      },
+    ],
+    "education": [
+      {
+        "institution": "CPC",
+        "department": "Electrical and electronics",
+        "studyType": "fulltime",
+        "batch start year": 2010,
+        "batch end year": 2013,
+        "Percentage": 72,
+      }
+    ],
+    "skills": [
+      {
+        "name": "oracle",
+        "level": "beginer",
+        "project": [
+          "update"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish,kannada",
+      }
+    ],
+    "interests": [
+      {
+        "name": "cooking,listening music,",
+      }
+    ]
+  }
+  console.log(myResume);
+  
